5701364605be864808523ebcfb54427c5ad8af01
Chacal <chacal_exodius@hotmail.com>
2022-11-05 18:56:18 -0300

    Add files via upload

---
+++ lib/lib/cadastrapageuser.dart
@@ -0,0 +1,147 @@
+import 'package:easywash/loginpage.dart';
+import 'package:flutter/material.dart';
+
+class CadastrarPageUser extends StatefulWidget {
+  const CadastrarPageUser({super.key});
+
+  @override
+  State<CadastrarPageUser> createState() => _CadastrarPageUserState();
+}
+
+class _CadastrarPageUserState extends State<CadastrarPageUser> {
+  final _formKey = GlobalKey<FormState>();
+  final _senhaController = TextEditingController();
+  final _nomeController = TextEditingController();
+  final _dnController = TextEditingController();
+  final _emailController = TextEditingController();
+  bool _verSenha = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Cadastro - Usuário',
+            style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+          ),
+          backgroundColor: const Color.fromRGBO(61, 144, 171, 1)),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            alignment: Alignment(0.0, -1.0),
+            image: AssetImage('assets/backgroud-prelogin.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Form(
+          key: _formKey,
+          child: Container(
+            margin: const EdgeInsets.only(right: 20, left: 20, top: 20),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nomeController,
+                  decoration: const InputDecoration(
+                    label: Text('Nome'),
+                    hintText: 'Nome Completo',
+                  ),
+                  validator: (nome) {
+                    if (nome == null || nome.isEmpty) {
+                      return 'Digite seu Nome Compelto';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _dnController,
+                  decoration: const InputDecoration(
+                    label: Text('Data Nascimento'),
+                    hintText: 'DD/MM/AAAA',
+                  ),
+                  validator: (dn) {
+                    if (dn == null || dn.isEmpty) {
+                      return 'Digite sua Data de Nascimento';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    label: Text('E-mail'),
+                    hintText: 'nome@email.com',
+                  ),
+                  validator: (email) {
+                    if (email == null || email.isEmpty) {
+                      return 'Digite seu E-mail';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _senhaController,
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: !_verSenha,
+                  decoration: InputDecoration(
+                    label: const Text('Senha'),
+                    hintText: 'Digite sua senha',
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _verSenha = !_verSenha;
+                        });
+                      },
+                      icon: Icon(
+                        _verSenha
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility_outlined,
+                      ),
+                    ),
+                  ),
+                  validator: (senha) {
+                    if (senha == null || senha.isEmpty) {
+                      return 'Digite sua Senha';
+                    } else if (senha.length < 8) {
+                      return 'Digite Uma Senha Maior';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      cadastrar();
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(216, 40),
+                    backgroundColor: const Color.fromARGB(255, 71, 212, 255),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text('Cadastrar',
+                      style: TextStyle(color: Colors.black)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  cadastrar() async {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const LoginPage(),
+      ),
+    );
+  }
+}
---
+++ lib/lib/homepage.dart
@@ -0,0 +1,72 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final dynamic title;
+  const HomePage({Key? key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Página Inicial',
+          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+        ),
+        backgroundColor: const Color.fromRGBO(61, 144, 171, 1),
+        leading: Image.asset('assets/IconTop.png'),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            alignment: Alignment(0.0, -1.0),
+            image: AssetImage('assets/backgroud-prelogin.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Form(
+          child: Container(
+            margin: const EdgeInsets.only(right: 20, left: 20, top: 20),
+            child: Column(
+              children: [
+                const Center(
+                  heightFactor: 2,
+                  child: Text('Pagina Inicial',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontFamily: 'Ubuntu',
+                        color: Color.fromARGB(255, 51, 51, 51),
+                      )),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _signOut();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(216, 40),
+                    backgroundColor: const Color.fromARGB(255, 71, 212, 255),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text(
+                    'LOGOUT',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+}
